fd82aee9362ac5d0b06c0a09d38c33a242803877
jcmchen <jeremychen2@gmail.com>
2025-12-10 02:23:25 -0800

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,8 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
-const isProd = process.env.NODE_ENV === "production";
-const REPO_NAME = "portfolio"
+// const isProd = process.env.NODE_ENV === "production";
+// const REPO_NAME = "portfolio"
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
@@ -22,25 +22,25 @@ const nextConfig: NextConfig = withMDX({
   // 其他 Next.js 設定可保留
   reactStrictMode: true,
 
-  // github pages
-  output: "export",
+  // // github pages
+  // output: "export",
 
   // next/image 在 export 模式下需要關掉內建 image optimization
   images: {
     unoptimized: true,
   },
 
-  // 部署到「子路徑」時要設定 basePath / assetPrefix
-  basePath: isProd ? `/${REPO_NAME}` : "",
-  assetPrefix: isProd ? `/${REPO_NAME}/` : "",
+  // // 部署到「子路徑」時要設定 basePath / assetPrefix
+  // basePath: isProd ? `/${REPO_NAME}` : "",
+  // assetPrefix: isProd ? `/${REPO_NAME}/` : "",
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
 
 });
 
